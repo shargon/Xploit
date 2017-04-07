c397d86e574a1a892245443190a44f17569a4655
Alvaro Diaz <alvarodiazher@gmail.com>
2017-04-07 12:03:40 +0200

    Subdomain Enumeration
    Important! Xploit needs the subdomain.txt file in the binary directory
    (This is a temporal issue)

---
+++ XPloit.Helpers/SubdomainEnumerationHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+
+
+namespace XPloit.Helpers
+{
+    public class SubdomainEnumerationHelper
+    {
+        public class SubdomainEnumerationOptions
+        {
+            /// <summary>
+            /// Include files
+            /// </summary>
+            public string[] domain { get; set; }
+        }
+
+        public static bool SubdomainCheck(string url)
+        {
+            try
+            {
+                IPHostEntry hostInfo = Dns.GetHostByName(url);
+                if (hostInfo.ToString() != "")
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+    }
+}
