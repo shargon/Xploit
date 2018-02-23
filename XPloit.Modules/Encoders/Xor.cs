a1b1e441683a270d0a13764fcdf950cf701af96a
Teeknofil <root@localhost.localdomain>
2018-02-22 23:53:28 -0500

    Add Xor Encoder,ToDo: Fix Target string for array bytes

---
+++ XPloit.Modules/Encoders/Xor.cs
@@ -0,0 +1,28 @@
+﻿using XPloit.Core;
+using XPloit.Core.Attributes;
+
+namespace Encoders.Xor
+{
+    [ModuleInfo(Author = "Teeknofil", Description = "Encode byte[] to Xor byte")]
+    public class XorEncoder : XPloit.Core.Encoder
+    {
+        /// <summary>
+        /// Encode
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="strKey"></param>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+       //TODO: fix Target String
+        public static byte[] Run(byte[] bytes, string strKey, Payload payload)
+        {
+            if (payload == null) return null;
+
+            int amount = 350;
+            byte[] key = System.Text.Encoding.ASCII.GetBytes(strKey);
+            for (int i = 0; i < bytes.Length; i++) bytes[i] ^= (byte)(key[i % key.Length] >> (i + amount + key.Length) & 255);
+            return bytes;
+        }
+
+    }
+}
