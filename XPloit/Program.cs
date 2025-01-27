c05600d721a420b5a99fbf838a2a69282555d8fc
Fernando Diaz Toledano <shargon@gmail.com>
2025-01-27 16:21:26 +0100

    Clean

--- XPloit/Program.cs
+++ XPloit/Program.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 #endif
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Reflection;
 using System.Text;
 using XPloit.Core.Collections;
